ca1ca8a0bb692242dc380e781a26619d42141c2e
karthikatl <karthyca.s@gmail.com>
2024-07-23 23:48:21 +0100

    changes remove action

--- next.config.mjs
+++ next.config.mjs
@@ -6,7 +6,6 @@ const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
   basename: isProd ?  '/portfolio' : '',
   output: 'export',
-  distDir: 'dist',
   typescript: {
     ignoreBuildErrors: true,
   },
