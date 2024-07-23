9c45a5ab3176ffd8c89e7873cdca7a766168ba92
karthikatl <karthyca.s@gmail.com>
2024-07-23 13:30:15 +0100

    changes

--- next.config.mjs
+++ next.config.mjs
@@ -4,8 +4,8 @@ const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
     output: 'export',
-    assetPrefix: isProd ? './portfolio/' : '',
-  basePath: isProd ? './portfolio' : '',
+    assetPrefix: isProd ? '/portfolio/' : '',
+  basePath: isProd ? '/portfolio' : '',
     typescript: {
         ignoreBuildErrors: true,
     }
